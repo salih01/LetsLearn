7b807629d06674311a28a6270ed75a80cbc5b612
Salih Çakmak <slhckmk0@gmail.com>
2024-07-08 23:09:32 +0300

    First

---
+++ LetsLearn/LetsLearn/LetsLearnApp.swift
@@ -0,0 +1,20 @@
+//
+//  LetsLearnApp.swift
+//  LetsLearn
+//
+//  Created by Mehmet Salih ÇAKMAK (Mobil Uygulamalar Uygulama Geliştirme Müdürlüğü) on 8.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct LetsLearnApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
