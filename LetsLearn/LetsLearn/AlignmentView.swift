93e982731ed5c0d3cbb7ead6db011d6d91b85167
Salih Çakmak <mehmetsalih.cakmak@vakifbank.com.tr>
2024-07-13 22:37:38 +0300

    Learn Alignment

---
+++ LetsLearn/LetsLearn/AlignmentView.swift
@@ -0,0 +1,18 @@
+//
+//  AlignmentView.swift
+//  LetsLearn
+//
+//  Created by Mehmet Salih ÇAKMAK (Mobil Uygulamalar Uygulama Geliştirme Müdürlüğü) on 13.07.2024.
+//
+
+import SwiftUI
+
+struct AlignmentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlignmentView()
+}
