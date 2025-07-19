85559e12a7c28badd9cee7e293ea3bd789684586
salih01 <slhckmk0@gmail.com>
2025-07-19 15:44:43 +0300

    Learn Swift UI

---
+++ LetsLearnSwiftUI/LetsLearnSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LetsLearnSwiftUI
+//
+//  Created by Mehmet Salih ÇAKMAK (Mobil Uygulamalar Uygulama Geliştirme Müdürlüğü) on 19.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LetsLearnSwiftUI/LetsLearnSwiftUI/LetsLearnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsLearnSwiftUIApp.swift
+//  LetsLearnSwiftUI
+//
+//  Created by Mehmet Salih ÇAKMAK (Mobil Uygulamalar Uygulama Geliştirme Müdürlüğü) on 19.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct LetsLearnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
