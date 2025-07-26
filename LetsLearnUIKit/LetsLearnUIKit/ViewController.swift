f12956baf8a33db9ceb6151cabc01db6f23ce537
salih01 <slhckmk0@gmail.com>
2025-07-26 21:42:03 +0300

    Create UIKit Project

---
+++ LetsLearnUIKit/LetsLearnUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LetsLearnUIKit
+//
+//  Created by Mehmet Salih ÇAKMAK (Mobil Uygulamalar Uygulama Geliştirme Müdürlüğü) on 26.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
